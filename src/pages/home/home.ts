26a1aa175a56d4d63fd506de97c0c8fe03cdb9e1
Teerasej <teerasej@gmail.com>
2017-07-11 10:35:59 +0700

    Implement explicit Headers and RequestOption to adapt request property

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
-import {Http} from '@angular/http';
+import {Http, Headers, RequestOptions} from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 
 @Component({
   selector: 'page-home',
@@ -23,8 +22,17 @@ export class HomePage {
     let url = "/post-string.php";
     console.log('Sending: ' + url);
 
+    
+    // In fact, Angular's Http will set Content-Type to 'application/json' as default
+    // We can modify to other content's type if needed.
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json')
 
-    this.http.post(url, {}).subscribe(res => {
+    let options = new RequestOptions();
+    options.headers = headers;
+
+
+    this.http.post(url, {}, options).subscribe(res => {
       
       console.log('Got it.');
       
